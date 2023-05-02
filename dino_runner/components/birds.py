a61de2e180ba956b31d5441ff36ae0ed3adec6e1
Jhon6723 <jhon.rivera@fundacion-jala.org>
2023-05-02 18:30:23 -0500

    modificaciones

--- dino_runner/components/birds.py
+++ dino_runner/components/birds.py
@@ -1,21 +1,44 @@
 import pygame
 from dino_runner.components.obstacles.obstacle import Obstacle
+from dino_runner.utils.constants import SCREEN_WIDTH
 import random
 class BirdDown(Obstacle):
     def __init__(self, image):
+        self.step_index = 0
         self.image = image
         self.type = random.randint(0, 1)
         super().__init__(image, self.type)
         self.rect.y = 280
+        
+    def draw(self, screen):
+        if self.step_index >= 9:
+            self.step_index = 0
+        screen.blit(self.image[self.step_index // 5], self.rect)
+        self.step_index += 1
 class BirdMid(Obstacle):
     def __init__(self, image):
+        self.step_index = 0
         self.image = image
-        self.type = random.randint(0, 1)
+        self.type = 0
         super().__init__(image, self.type)
         self.rect.y = 220
+        
+    def draw(self, screen):
+        if self.step_index >= 9:
+            self.step_index = 0
+        screen.blit(self.image[self.step_index // 5], self.rect)
+        self.step_index += 1
 class BirdUp(Obstacle):
     def __init__(self, image):
+        self.step_index = 0
         self.image = image
-        self.type = random.randint(0, 1)
+        self.type = 0
         super().__init__(image, self.type)
         self.rect.y = 170
+        self.rect.x = SCREEN_WIDTH
+    
+    def draw(self, screen):
+        if self.step_index >= 9:
+            self.step_index = 0
+        screen.blit(self.image[self.step_index // 5], self.rect)
+        self.step_index += 1
